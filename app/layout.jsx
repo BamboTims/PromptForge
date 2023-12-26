b6a8381d32cc6450cd09df31aa692454623ca0b2
Bambotims <timilehinadebambo@gmail.com>
2023-12-26 01:10:53 +0100

    initial-commit

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import "@styles/global.css";
+
+import Nav from "@components/Nav/Nav";
+import Provider from "@components/Provider/Provider";
+
+export const metadata = {
+  title: "PromptForge",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import Feed from "@components/Feed/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Share your favourite Prompts</p>
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
